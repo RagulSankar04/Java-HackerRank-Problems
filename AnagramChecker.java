d29a52f0cc44ff993f1dc239cf00845076d64338
Ragulsankar04 <ragulsankar2004@gmail.com>
2025-05-21 11:55:58 +0530

    String and Regex Completed

--- AnagramChecker.java
+++ AnagramChecker.java
@@ -2,8 +2,9 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class AnagramChecker{
-    static boolean AnagramCheck(String a, String b){
+public class AnagramChecker {
+
+    static boolean AnagramCheck1(String a, String b) {
         char[] arr1 = a.toCharArray();
         char[] arr2 = b.toCharArray();
 
@@ -13,30 +14,94 @@ public class AnagramChecker{
         return Arrays.equals(arr1, arr2);
 
     }
-    static boolean isAnagram(String a, String b) {
+
+    static boolean AnagramCheck2(String a, String b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
         if (a.length() != b.length()) {
             return false;
         }
+
+        int[] charCount = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            int countA = 0;
+            int countB = 0;
+            for (int j = 0; j < a.length(); j++) {
+                if (a.charAt(i) == a.charAt(j)) {
+                    countA++;
+                }
+                if (a.charAt(i) == b.charAt(j)) {
+                    countB++;
+                }
+            }
+            if (countA == countB) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean AnagramCheck3(String a, String b) {
         a = a.toLowerCase();
         b = b.toLowerCase();
-        
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        int[] charCount = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            charCount[a.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < b.length(); i++) {
+            charCount[b.charAt(i) - 'a']--;
+        }
+        for (int num : charCount) {
+            if (num != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isAnagram(String a, String b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        if (a.length() != b.length()) {
+            return false;
+        }
+
         for (int i = 0; i < b.length(); i++) {
             a = a.replaceFirst(String.valueOf(b.charAt(i)), "");
         }
 
-        return a.isEmpty() ? true : false;
-            
+        if (a.isEmpty()) {
+            return true;
         }
+        return false;
+
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
         String b = sc.nextLine();
         sc.close();
+
         boolean res = isAnagram(a, b);
         System.out.println((res) ? "Anagram" : "Not Anagrams");
 
-        boolean val = AnagramCheck(a, b);
-        System.out.println((val) ? "Anagram" : "Not Anagram");
+        boolean val1 = AnagramCheck1(a, b);
+        System.out.println((val1) ? "Anagram" : "Not Anagram");
+
+        boolean val2 = AnagramCheck2(a, b);
+        System.out.println((val2) ? "Anagram" : "Not Anagram");
+
+        boolean val3 = AnagramCheck3(a, b);
+        System.out.println((val3) ? "Anagram" : "Not Anagram");
 
     }
 }
--- SubstringComparison.java
+++ SubstringComparison.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
 public class SubstringComparison {
 
     public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
 
-        smallest = s.substring(0, k);
-        largest = s.substring(0, k);
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
         for (int i = 0; i <= s.length() - k; i++) {
             String str = s.substring(i, k + i);
             if (smallest.compareTo(str) > 0) {
@@ -19,9 +17,6 @@ public class SubstringComparison {
             }
         }
         return smallest + "\n" + largest;
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
 
     }
 
@@ -32,5 +27,6 @@ public class SubstringComparison {
         scan.close();
 
         System.out.println(getSmallestAndLargest(s, k));
+        
     }
 }
